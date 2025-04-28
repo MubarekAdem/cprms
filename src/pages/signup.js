5d593a4672cc5555e4e69b00836b4e213e141539
Mubarek Adem <mubarekademy@gmail.com>
2025-04-28 07:56:22 +0300

    removed navbar in /signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -75,7 +75,7 @@ export default function Signup() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200">
-      <Navbar showBackButton={true} />
+      {/* <Navbar showBackButton={true} /> */}
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
           <Card className="w-full max-w-md">
