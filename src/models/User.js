cde83bb00ec58fa93b04892c2946a343df136d7a
Mubarek Adem <mubarekademy@gmail.com>
2025-05-02 23:11:45 +0300

    added reset password for first aid app

--- src/models/User.js
+++ src/models/User.js
@@ -27,6 +27,8 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
   },
   { timestamps: true }
 );
