9a5ed056429707fca3a50c24a2e1cc026c249b69
Mubarek Adem <mubarekademy@gmail.com>
2025-05-18 22:49:20 +0300

    renamed to reset-password-web

--- src/pages/reset-password.js
+++ src/pages/reset-password.js
@@ -44,7 +44,7 @@ export default function ResetPassword() {
     }
 
     try {
-      const res = await fetch("/api/auth/reset-password", {
+      const res = await fetch("/api/auth/reset-password-web", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, otp, password }),
